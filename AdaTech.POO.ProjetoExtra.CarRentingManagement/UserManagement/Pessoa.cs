5387346e3955dad89621539a8699b6c05110f782
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-09 20:47:49 -0300

    feat: User Management folder

---
+++ AdaTech.POO.ProjetoExtra.CarRentingManagement/UserManagement/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.POO.ProjetoExtra.CarRentingManagement.UserManagement
+{
+    internal abstract class Pessoa
+    {
+        public decimal Id { get; private set; }
+        public string CPF { get; private set; }
+        public string Name { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+        public Address Address { get; private set; }
+        public string Senha { get; private set; }
+
+
+    }
+}
