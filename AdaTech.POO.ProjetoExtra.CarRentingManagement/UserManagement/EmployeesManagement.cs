9df20d25118faefcce5afc24fbf7729f7c545122
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-10 01:04:15 -0300

    feat: employees management class

---
+++ AdaTech.POO.ProjetoExtra.CarRentingManagement/UserManagement/EmployeesManagement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.POO.ProjetoExtra.CarRentingManagement.UserManagement
+{
+    internal class EmployeesManagement
+    {
+        private static List<Employee> employees;
+
+        internal static Employee Login(string email, string password)
+        {
+            foreach (var employee in employees)
+            {
+                if (employee.Email.Equals(email.ToLower()))
+                {
+                    if (employee.CheckPassword(password))
+                        return employee;
+                    return null;
+                }
+            }
+
+            return null;
+        }
+    }
+}
