6d4f10b08d8f90c0f1373cf154f16106523eafbd
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-09 23:24:43 -0300

    feat: Manager

--- AdaTech.POO.ProjetoExtra.CarRentingManagement/UserManagement/Manager.cs
+++ AdaTech.POO.ProjetoExtra.CarRentingManagement/UserManagement/Manager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AdaTech.POO.ProjetoExtra.CarRentingManagement.Exceptions;
+using AdaTech.POO.ProjetoExtra.CarRentingManagement.RentalManagement.PaymentManagement;
+using AdaTech.POO.ProjetoExtra.CarRentingManagement.UserManagement.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,24 @@ using System.Threading.Tasks;
 
 namespace AdaTech.POO.ProjetoExtra.CarRentingManagement.UserManagement
 {
+    using Exceptions;
     internal class Manager : Employee
     {
+        public decimal Bonus { get; private set; }
+        public Manager(decimal bonus, decimal id, string CPF, string name, string phoneNumber, string email, 
+            Address address, string password, Department department, string position, decimal salary) 
+                : base(id, CPF, name, phoneNumber, email, address, password, department, position, salary)
+        {
+            this.Bonus = bonus;
+        }
+
+        public void UpdateBonus(decimal bonus)
+        {
+            if(bonus == 0)
+                throw new EmptyValueException("Bonus cannot be zero.");
+            this.Bonus = bonus;
+        }
+
+
     }
 }
