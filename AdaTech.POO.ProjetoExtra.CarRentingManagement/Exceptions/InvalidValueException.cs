416a9b8e7885bf271110911fdab78a2fe271b6c2
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-10 01:05:39 -0300

    feat: custom exceptions folder

---
+++ AdaTech.POO.ProjetoExtra.CarRentingManagement/Exceptions/InvalidValueException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.POO.ProjetoExtra.CarRentingManagement.Exceptions
+{
+    internal class InvalidValueException : Exception
+    {
+        public InvalidValueException(string msg) : base (msg){ }
+    }
+}
